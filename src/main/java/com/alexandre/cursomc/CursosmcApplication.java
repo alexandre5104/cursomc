be32292eb51b9afb1007b1caa122129084028025
Alexandre Batista <alexandrebatistaril@gmail.com>
2020-09-30 20:37:19 -0300

    Projeto criado

---
+++ src/main/java/com/alexandre/cursomc/CursosmcApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosmcApplication.class, args);
+	}
+
+}
