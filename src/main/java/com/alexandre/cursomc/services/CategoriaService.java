dc926115413f56a861a7e4c0724bbcde170116f1
Alexandre Batista <alexandrebatistaril@gmail.com>
2020-10-12 22:56:48 -0300

    Criando CategoriaService e CategoriaRepository

---
+++ src/main/java/com/alexandre/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.alexandre.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandre.cursomc.model.Categoria;
+import com.alexandre.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		return categoria.orElse(null);
+	}
+}
